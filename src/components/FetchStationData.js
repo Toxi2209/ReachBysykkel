dbe5fa6b2017a4973aa9badc48aa63408504ca94
jesi2209 <jesi2209@hotmail.com>
2021-08-29 12:22:19 +0200

    Inital push

---
+++ src/components/FetchStationData.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./FetchStationData.css"
+
+
+export default class FetchStationData extends React.Component {
+  state = {
+    loading: true,
+    station: null
+  };
+
+
+  async componentDidMount() {
+    const url = "https://gbfs.urbansharing.com/oslobysykkel.no/station_information.json";
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({ station: data.data.stations, loading: false });
+  }
+  
+
+  render() {
+    if (this.state.loading) {
+      return <div>loading...</div>;
+    }
+
+    if (!this.state.station.length) {
+      return <div>didn't get a station</div>;
+    }
+
+    return (
+      <div>
+        {this.state.station.map((station) => (
+            <ul key={station.station_id}>
+              <div style={{fontWeight: "bold"}}>Name:{station.name}</div>
+              <div>Stasjon id: {station.station_id}</div>
+            </ul>
+        ))}
+      </div>
+    );
+  }
+}
---
+++ src/components/FetchStationStatus.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import "./FetchStationStatus.css"
+
+
+export default class FetchStationStatus extends React.Component {
+  state = {
+    loading: true,
+    station: null
+  };
+
+
+  async componentDidMount() {
+    const url = "https://gbfs.urbansharing.com/oslobysykkel.no/station_status.json";
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({ station: data.data.stations, loading: false });
+  }
+  
+
+  render() {
+    if (this.state.loading) {
+      return <div>loading...</div>;
+    }
+
+    if (!this.state.station.length) {
+      return <div>didn't get a station</div>;
+    }
+
+    return (
+      <div>
+        {this.state.station.map((station) => (
+            <ul key={station.station_id}>
+                <div>Ledige Docks: {station.num_docks_available} </div>
+               <div>Ledige sykler:{station.num_bikes_available}</div>
+            </ul>
+ 
+        ))}
+      </div>
+    );
+  }
+}
