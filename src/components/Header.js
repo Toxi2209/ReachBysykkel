008f3644c9e1087e4e65bd5f5256ddc2a684ecae
jesi2209 <jesi2209@hotmail.com>
2021-08-30 13:38:39 +0200

    sykler er med en k

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ import './Header.css';
 function Header() {
   return (
     <div>
-      <FaBiking size="2em" color="green" style={{ paddingTop:"2em", paddingLeft: "3em"}}/> <h1>BySykkler</h1>
+      <FaBiking size="2em" color="green" style={{ paddingTop:"2em", paddingLeft: "3em"}}/> <h1>BySykler</h1>
     </div>
     
   )
